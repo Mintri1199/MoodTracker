763e476d15ea9c30d6828489823b2758eb1ecc75
Jackson Ho <mintri1199@gmail.com>
2018-11-22 14:43:11 -0800

    Initial Commit

---
+++ MoodTrackerProg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoodTrackerProg
+//
+//  Created by Jackson Ho on 11/22/18.
+//  Copyright © 2018 Jackson Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
